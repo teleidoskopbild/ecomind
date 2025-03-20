e026a5e2d99ae6423a3b8cde9b13c249b0603eb0
teleidoskopbild <teleidoskopbild@gmail.com>
2025-03-20 22:21:49 +0100

    add portfoliopage with flipcards

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,4 +1,33 @@
+import portfolio from "../data/portfolio.json";
+import "../styles/portfolio.css";
+
 function PortfolioPage() {
-  return <div className="h-screen w-full bg-green-500"></div>;
+  return (
+    <div className="min-h-screen bg-green-400 flex flex-col justify-center items-center">
+      <h1 className="text-6xl font-bold text-white text-center mt-24 sm:mt-12">
+        OUR PORTFOLIO
+      </h1>
+      <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-5xl">
+        {portfolio.map((portfolio, index) => (
+          <div key={index} className="card relative w-60 h-60">
+            <div className="card_content w-full h-full text-center transition-transform duration-1000 text-white font-bold">
+              <div className="card_front rounded-lg absolute top-0 bottom-0 right-0 left-0 flex items-center justify-center">
+                <h2 className="absolute text-white text-4xl font-bold">
+                  {portfolio.back.title}
+                </h2>
+                <img
+                  className="w-full h-full object-cover rounded-lg"
+                  src={portfolio.front.src}
+                />
+              </div>
+              <div className="card_back rounded-lg absolute top-0 bottom-0 right-0 left-0 p-8 bg-green-600 flex items-center justify-center">
+                <h2>{portfolio.back.description}</h2>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default PortfolioPage;
