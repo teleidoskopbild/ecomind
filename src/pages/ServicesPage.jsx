108ad276e0870f21c99d620e4e64b84698a2cf17
teleidoskopbild <teleidoskopbild@gmail.com>
2025-03-21 17:00:21 +0100

    hotfix

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function MotionProject() {
+export default function Services() {
   const [counter, setCounter] = useState(0);
   let scrollCount = 0;
 
