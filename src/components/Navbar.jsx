2c14fdc8f36db98f49ca64e9085b9b8512a325ef
teleidoskopbild <teleidoskopbild@gmail.com>
2025-04-01 22:10:23 +0200

    style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,50 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="fixed top-0 z-10 bg-green-700 p-3 w-full text-white text-lg">
-      <ul className="flex gap-8">
-        <li className="hover:scale-110 transition-transform duration-300">
-          <Link to="/">About Us</Link>
-        </li>
-        <li className="hover:scale-110 transition-transform duration-300">
-          <Link to="/portfolio">Portfolio</Link>
-        </li>
-        <li className="hover:scale-110 transition-transform duration-300">
-          <Link to="/services">Services</Link>
-        </li>
-        <li className="hover:scale-110 transition-transform duration-300">
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+      <div className="flex items-center cursor-pointer">
+        <button
+          onClick={toggleMenu}
+          className={`cursor-pointer text-white ${isOpen ? "hidden" : ""}`}
+        >
+          <div className="w-6 h-1 bg-white mb-1"></div>
+          <div className="w-6 h-1 bg-white mb-1"></div>
+          <div className="w-6 h-1 bg-white mb-1"></div>
+        </button>
+        <ul
+          className={`flex flex-col gap-8 items-center w-full h-screen justify-center text-xl sm:text-3xl text-bold ${
+            !isOpen ? "hidden" : ""
+          }`}
+        >
+          <li className="hover:scale-110 transition-transform duration-300">
+            <Link onClick={toggleMenu} to="/">
+              About Us
+            </Link>
+          </li>
+          <li className="hover:scale-110 transition-transform duration-300">
+            <Link onClick={toggleMenu} to="/portfolio">
+              Portfolio
+            </Link>
+          </li>
+          <li className="hover:scale-110 transition-transform duration-300">
+            <Link onClick={toggleMenu} to="/services">
+              Services
+            </Link>
+          </li>
+          <li className="hover:scale-110 transition-transform duration-300">
+            <Link onClick={toggleMenu} to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
