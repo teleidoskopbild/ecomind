edc714c5b755ecb8282a76782775feaa32ba45ac
teleidoskopbild <teleidoskopbild@gmail.com>
2025-04-26 22:22:38 +0200

    add navbar slide-in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     setIsOpen(!isOpen);
   };
   return (
-    <nav className="fixed top-0 z-10 bg-green-700 p-3 w-full text-white text-lg">
+    <nav className="fixed top-0 z-10 bg-green-700 p-3 w-full text-white text-lg transition-all duration-300 ease-in-out">
       <div className="flex items-center cursor-pointer">
         <button
           onClick={toggleMenu}
@@ -19,8 +19,8 @@ function Navbar() {
           <div className="w-6 h-1 bg-white mb-1"></div>
         </button>
         <ul
-          className={`flex flex-col gap-8 items-center w-full h-screen justify-center text-xl sm:text-3xl text-bold ${
-            !isOpen ? "hidden" : ""
+          className={`flex flex-col gap-8 items-center w-full justify-center text-xl sm:text-3xl text-bold transition-transform duration-300 ease-in-out ${
+            !isOpen ? "h-5 -translate-y-100" : "h-screen translate-y-0"
           }`}
         >
           <li className="hover:scale-110 transition-transform duration-300">
